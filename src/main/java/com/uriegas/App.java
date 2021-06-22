f721b4691d7ba871d6bace24abd385b6cde2d19f
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-06-21 21:25:06 -0500

    Rename files

--- src/main/java/com/uriegas/App.java
+++ src/main/java/com/uriegas/App.java
@@ -1,35 +1,31 @@
 package com.uriegas;
-import java.util.*;
-import javax.mail.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import java.net.URL;
 
-/**
- * JavaFX App
- */
-public class App{
+public class App extends Application {
+
     public static void main(String[] args) {
+        launch(args);
+    }
 
-        final String fromEmail = "1930526@upv.edu.mx"; //requires valid gmail id
-		final String password = ""; // correct password for gmail id
-		final String toEmail = "jesusuriegas18@gmail.com"; // can be any email id 
-		
-		System.out.println("SSLEmail Start");
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
-		props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
-		props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
-		props.put("mail.smtp.port", "465"); //SMTP Port
-		
-		Authenticator auth = new Authenticator() {
-			//override the getPasswordAuthentication method
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(fromEmail, password);
-			}
-		};
-		
-		Session session = Session.getDefaultInstance(props, auth);
-		System.out.println("Session created");
-	        EmailUtil.sendEmail(session, toEmail,"SSLEmail Testing Subject", "SSLEmail Testing Body");
-   }
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            //URL path = new URL("file:src/main/resources/Login.fxml");
+            URL path = new URL("file:src/main/resources/Autenticacion.fxml");
+            loader.setLocation(path);
+            Scene scene = loader.load();
+
+            primaryStage.setTitle("Massive Email");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
